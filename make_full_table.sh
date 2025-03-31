036b11e096cf1e743bc0e1d2ee77424daad1fb52
Scott Buckley <scott@buck.ly>
2025-03-31 13:11:01 +0200

    minor updates for running on ubuntu

--- make_full_table.sh
+++ make_full_table.sh
@@ -1,7 +1,14 @@
 rm ./page/table.html
 
 function run_sable () {
-./sable --count-det --output=table.html --report-table --input-tlsf=$1
+# ./sable --count-det --output=table.html --report-table --input-tlsf=$1
+filename=$(basename $1)
+echo
+echo
+echo $filename ...
+timeout 1h /usr/bin/time -o script_logs/$filename.time.log -v ./sable --output=table.html --report-table --input-tlsf=$1 > script_logs/$filename.output.log
+cat script_logs/$filename.time.log
 }
 
-find ../benchmarks_ulb -name "*.tlsf" | while read line; do run_sable $line; done
+# find benchmarks_ulb -name "*.tlsf" | while read line; do run_sable $line; done
+find ../benchmarks/tlsf/generated_tlsf -name "*.tlsf" | sort | while read line; do run_sable $line; done
